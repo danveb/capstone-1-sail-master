8dd0b8cd42d014bd26fa28ffbfc1dd3226a0e724
Daniel Bae <danibae91@icloud.com>
2021-07-18 13:49:49 -0700

    rework code on app.py and seed.py for Heroku

--- seed.py
+++ seed.py
@@ -1,8 +1,8 @@
 """Seed Database with sample data from CSV File"""
 
 from csv import DictReader
-from app import db 
-from models import Club
+from app import db, app 
+from models import Club, db 
 
 db.drop_all() 
 db.create_all() 
